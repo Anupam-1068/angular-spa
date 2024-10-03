3d96e77c78a702cb4af6a1d391350f2e8f41a96c
Anupam-1068 <107940692+Anupam-1068@users.noreply.github.com>
2024-10-03 15:30:16 +0300

    added home

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router'; // Import the Router
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
+  standalone: true, // If standalone
+  imports: [ReactiveFormsModule, ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) { // Inject the Router
+  constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
@@ -21,18 +23,12 @@ export class LoginComponent implements OnInit {
 
   onSubmit(): void {
     if (this.loginForm.valid) {
-      // Handle the login logic, e.g., authenticate with a server, etc.
-      console.log('Login successful:', this.loginForm.value);
-
-      // Redirect to the main page after successful login
-      this.router.navigate(['/']); // Redirect to the main page (or any other route)
-    } else {
-      console.log('Form is invalid');
+      // Handle the login logic
+      console.log(this.loginForm.value);
     }
   }
 
   handleForgotPassword(): void {
-    // Handle forgot password logic
-    console.log('Forgot Password clicked');
+    // Handle the forgot password logic
   }
 }
